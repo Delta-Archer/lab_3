510a3a0bfdf8c1a9ae54672512d88a8f5259ddc0
Delta-Archer <71624858+Delta-Archer@users.noreply.github.com>
2020-11-11 11:45:34 +0200

    Add files via upload

---
+++ Models/Mobiles.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab_3.Models
+{
+    public class Mobiles
+    {
+        public int Id { get; set; }
+        public int? CompanyId { get; set; }
+        public Companies Company { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Models/Purchases.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab_3.Models
+{
+    public class Purchases
+    {
+        public int Id { get; set; }
+        public string Person { get; set; }
+        public string PurchaseDate { get; set; }
+        public string MobileId { get; set; }
+        public int ShopId { get; set; }
+    }
+}
