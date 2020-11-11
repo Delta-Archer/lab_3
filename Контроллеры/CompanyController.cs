1a36827fb67c05489a9f8a1810f50fbdb7770e6f
Delta-Archer <71624858+Delta-Archer@users.noreply.github.com>
2020-11-11 11:44:15 +0200

    Add files via upload

---
+++ Контроллеры/CompanyController.cs
@@ -0,0 +1,30 @@
+﻿using Lab_3.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Lab_3.Controllers
+{
+    public class CompanyController : Controller
+    {
+        // GET: Company
+        MobileContext db = new MobileContext();
+        public ActionResult Index()
+        {
+            return View(db.Companies);
+        }
+        public ActionResult CompanyDetails(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Companies comp = db.Companies.Find(id);
+            if (comp == null)
+            {
+                return HttpNotFound();
+            }
+            comp.Mobiles = db.Mobiles.Where(m => m.CompanyId == comp.Id);
+            return View(comp);
+        }
+    }
+}
