fb28f04bbdeab62091df31240de8e1c77422adfc
Delta-Archer <71624858+Delta-Archer@users.noreply.github.com>
2020-11-18 11:39:06 +0200

    Add files via upload

--- Models/Companies.cs
+++ Models/Companies.cs
@@ -1,10 +1,12 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lab_3.Models
 {
     public class Companies
     {
         public int Id { get; set; }
+        [Display(Name = "Компанія")]
         public string Name { get; set; }
         public string Country { get; set; }
         public int Year { get; set; }
---
+++ Models/MobilePropertyValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_3.Models
+{
+    public class MobilePropertyValidator : ModelValidator
+    {
+        public MobilePropertyValidator(ModelMetadata metadata, ControllerContext context) : base(metadata, context) { }
+        public override IEnumerable<ModelValidationResult> Validate(object container)
+
+        {
+            Lab_3.Models.Mobiles b = container as Lab_3.Models.Mobiles;
+            if (b != null)
+            {
+
+            }
+            return Enumerable.Empty<ModelValidationResult>();
+        }
+
+    }
+}
